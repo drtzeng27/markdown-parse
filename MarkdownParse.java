838ef16f0ddb255afcc28f8b88534524667b774a
Dario Tzeng <drtzeng27@gmail.com>
2022-01-27 14:24:52 -0800

    Update MarkdownParse.java
    fixed bug where it returns an image file

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -12,10 +12,16 @@ public class MarkdownParse {
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
-            if (nextOpenBracket == -1) break;
+            if (nextOpenBracket == -1) break; //check if there is another link
             int nextCloseBracketOpenParen = markdown.indexOf("](", nextOpenBracket);
-            //int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", nextCloseBracketOpenParen);
+
+            //check if it is an image
+            if (nextOpenBracket > 0 && markdown.charAt(nextOpenBracket - 1) == '!') {
+                currentIndex = closeParen + 1;
+                continue;
+            }
+
             toReturn.add(markdown.substring(nextCloseBracketOpenParen + 2, closeParen));
             currentIndex = closeParen + 1;
         }
