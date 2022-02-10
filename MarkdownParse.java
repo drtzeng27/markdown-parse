b05b76041afe51f7aa6dd8e6c4a522b002f369d7
Dario R Tzeng <cs15lwi22aut@ieng6-201.ucsd.edu>
2022-02-10 15:51:38 -0800

    adding a makefile

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -9,6 +9,7 @@ public class MarkdownParse {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then take up to
         // the next )
+        // change
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
             
